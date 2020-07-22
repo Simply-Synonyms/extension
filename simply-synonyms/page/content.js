0f75c8c7b1edd2ef641994b4b4f21d5a91771b1f
Benjamin Ashbaugh <scitronboy@gmail.com>
2020-07-22 16:01:14 -0600

    started synonym popup box

--- simply-synonyms/page/content.js
+++ simply-synonyms/page/content.js
@@ -1,5 +1,26 @@
-import popupHTML from './synonym_popup_html'
+// const script = document.createElement('script');
+// script.setAttribute("type", "module");
+// script.setAttribute("src", chrome.extension.getURL('page/main.js'));
+// const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+// head.insertBefore(script, head.lastChild);
+
+const popupHtml = `
+<div id="popup">
+    <div class="definition">The definition of this word is....</div>
+    <div class="synonyms">
+        SYNONYM
+    </div>
+</div>
+`;
+
+function getSynonyms(word) {
+  fetch(`https://simply-synonyms-api.herokuapp.com/api/get-synonyms?word=${word}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
 
 let popupDiv = document.createElement('div')
+popupDiv.innerHTML = popupHtml
+document.body.appendChild(popupDiv)
 
-popupDiv.innerHTML = popupHTML
+getSynonyms('cat')
--- simply-synonyms/page/synonym_popup_html.js
+++
@@ -1,8 +0,0 @@
-export default `
-<div>
-    <div class="definition">The definition of this word is....</div>
-    <div class="synonyms">
-        SYNONYM
-    </div>
-</div>
-`
