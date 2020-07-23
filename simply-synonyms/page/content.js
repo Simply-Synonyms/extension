48061736ca02cabd495f750765661983a253392b
Benjamin Ashbaugh <scitronboy@gmail.com>
2020-07-22 20:57:59 -0600

    started basic popup box

--- simply-synonyms/page/content.js
+++ simply-synonyms/page/content.js
@@ -1,14 +1,11 @@
-// const script = document.createElement('script');
-// script.setAttribute("type", "module");
-// script.setAttribute("src", chrome.extension.getURL('page/main.js'));
-// const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-// head.insertBefore(script, head.lastChild);
 
 const popupHtml = `
 <div id="popup">
-    <div class="definition">The definition of this word is....</div>
-    <div class="synonyms">
-        SYNONYM
+    <div class="header">
+      <div>Synonyms:</div>
+      <div class="right-button" id="close-button">x</div>
+    </div>
+    <div class="synonyms" id="synonyms">
     </div>
 </div>
 `;
@@ -22,15 +19,28 @@ function getSynonyms(word) {
     });
 }
 
-document.body.addEventListener('dblclick', (e) => {
-  let text = ''
-  text = window.getSelection().toString()
-  getSynonyms(text)
-    .then((response) => {
-      alert(response.synonyms.toString())
-    })
-})
+function addExtension() {
+  document.body.addEventListener('dblclick', (e) => {
+    let text = ''
+    text = window.getSelection().toString()
+    getSynonyms(text)
+      .then((response) => {
+        const flattenedSynonyms = response.synonyms.flat()
+        const synonymsDiv = document.getElementById('synonyms')
+        synonymsDiv.innerHTML = ''
+        for (const syn of flattenedSynonyms) {
+          const synEl = document.createElement('div')
+          synEl.innerText = syn
+          synonymsDiv.appendChild(synEl)
+        }
+      })
+  })
 
-let popupDiv = document.createElement('div')
-popupDiv.innerHTML = popupHtml
-document.body.appendChild(popupDiv)
+  let popupDiv = document.createElement('div')
+  popupDiv.innerHTML = popupHtml
+  document.body.appendChild(popupDiv)
+}
+
+chrome.storage.local.get(['option_popupDisabled'], (result) => {
+  if (!result.option_popupDisabled) addExtension()
+})
