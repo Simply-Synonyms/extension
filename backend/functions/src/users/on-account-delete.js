ee9498252599d170302ffca3fd2d8a547f5e7202
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-10-15 23:16:55 -0600

    update

--- backend/functions/src/users/on-account-delete.js
+++
@@ -1,7 +0,0 @@
-const mail = require('../util/email')
-
-module.exports = (user) => {
-  mail.removeUserFromList(user.email, 'users')
-  mail.removeUserFromList(user.email, 'announcements')
-  return admin.firestore().collection('users').doc(user.uid).delete()
-}
